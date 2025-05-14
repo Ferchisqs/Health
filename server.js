0606079f712a8e798063090d4a39daeab9d0e971
Ferchisqs <selihgamer6@gmail.com>
2025-05-13 21:46:00 -0600

    change file server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Hola fernanda' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Hola fernanda 2.0' });
 });
 
 const PORT = process.env.PORT || 3000;
